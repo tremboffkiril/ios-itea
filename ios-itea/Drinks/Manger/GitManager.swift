0900438b8fab875447d280e7492d8cbcfca6d1c3
Trembovetskiy Kiril <tremboffkirya96@gmail.com>
2019-04-18 21:17:16 +0300

    Make GITManager

---
+++ ios-itea/Drinks/Manger/GitManager.swift
@@ -0,0 +1,14 @@
+//
+//  GitManager.swift
+//  ios-itea
+//
+//  Created by Kiril Trembovetskiy on 4/18/19.
+//  Copyright © 2019 ITEA. All rights reserved.
+//
+
+import Foundation
+
+class GitManager {
+    var gitURL: String?
+    var gitMaster: String?
+}
